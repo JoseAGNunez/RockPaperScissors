d42a1e863abb5929077a6c3b3fee46aa33102f15
Angel Nunez <joseagnunez@gmail.com>
2022-12-29 21:05:01 -0800

    add function to play a round of r/p/s vs computer

--- javascript.js
+++ javascript.js
@@ -8,3 +8,21 @@ function getComputerChoice() {
         return "Scizzors";
     };
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+        return "Tie Game.";
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER") {
+        return "You Lose! Paper beats rock.";
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCIZZORS") {
+        return "You Win! Rock beats scizzors.";
+    } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() == "ROCK") {
+        return "You win! Paper beats rock.";
+    } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCIZZORS") {
+        return "You Lose! Scizzors beats paper.";
+    } else if (playerSelection.toUpperCase() === "SCIZZORS" && computerSelection.toUpperCase() === "ROCK") {
+        return "You Lose! Rock beats scizzors.";
+    } else if (playerSelection.toUpperCase() === "SCIZZORS" && computerSelection.toUpperCase() === "PAPER") {
+        return "You win! Scizzors beats paper.";
+    };
+}
