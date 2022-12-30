7c74b93a9741dbba71667374cef9c9584016fcf8
Angel Nunez <joseagnunez@gmail.com>
2022-12-29 19:41:40 -0800

    add function to decide computer's choice of r/p/s

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+    if (randomNum === 0) {
+        return "Rock";
+    } else if (randomNum === 1) {
+        return "Paper";
+    } else {
+        return "Scizzors";
+    };
+}
